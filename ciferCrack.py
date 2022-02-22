c5d3f3a7f869fd994ea63fbdfd815af551105faa
alex99op <alexander.ronasen@gmail.com>
2022-02-22 11:42:12 +0100

    Update V0.0.3
    Changed language to all English, made the code more pretty.

--- ciferCrack.py
+++ ciferCrack.py
@@ -1,12 +1,16 @@
-
 alfabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+decrypted_message = []
 
-user_password = input("Skriv in passord\n")
-user_password = list(user_password) #Her blir stringen gjort om til en array.
-user_yey = input("\nVet du hvor mange hopp?(Y/N)\n")
-user_hopp = input("Hvor mange hopp?\n")
-new_password = []
+encrypted_message = input("Write inn the encrypted message\n")
+encrypted_message = list(encrypted_message) #The string is converted to an array.
 
+user_yey = input("Do you know how many jumps?(Y/N)\n")
+if user_yey == "y" or user_yey == "Y":
+    user_jump = input("How many jumps?(Write a number)\n")
+    print(user_jump)
+    
+
+print(encrypted_message)
 
 #try 
 #if user_yey == Y: 
@@ -14,6 +18,6 @@ new_password = []
 #else user_yey == N:
 #    bruteforce
 
-#for x, i in enumerate (user_password):
- #   if i == user_password[x]:
+#for x, i in enumerate (encrypted_message):
+ #   if i == encrypted_message[x]:
   #      print("Hello")
