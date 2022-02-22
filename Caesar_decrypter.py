f7928ca3afbcbe659fe1b0a6c6951855dc6bc457
alex99op <alexander.ronasen@gmail.com>
2022-02-22 11:47:50 +0100

    Better name

---
+++ Caesar_decrypter.py
@@ -0,0 +1,23 @@
+alfabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+decrypted_message = []
+
+encrypted_message = input("Write inn the encrypted message\n")
+encrypted_message = list(encrypted_message) #The string is converted to an array.
+
+user_yey = input("Do you know how many jumps?(Y/N)\n")
+if user_yey == "y" or user_yey == "Y":
+    user_jump = input("How many jumps?(Write a number)\n")
+    print(user_jump)
+    
+
+print(encrypted_message)
+
+#try 
+#if user_yey == Y: 
+    
+#else user_yey == N:
+#    bruteforce
+
+#for x, i in enumerate (encrypted_message):
+ #   if i == encrypted_message[x]:
+  #      print("Hello")
