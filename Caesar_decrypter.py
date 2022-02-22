f5bdac84f9590cf600ab3460bae3e7cc3b3634bf
alex99op <alexander.ronasen@gmail.com>
2022-02-22 13:25:28 +0100

    V0.0.5

--- Caesar_decrypter.py
+++ Caesar_decrypter.py
@@ -1,17 +1,26 @@
 alfabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-decrypted_message = []
 
 encrypted_message = input("Write inn the encrypted message\n")
-encrypted_message = list(encrypted_message) #The string is converted to an array.
+encrypted_message = list(encrypted_message) #The out is converted to an array.
+decrypted_message = encrypted_message
+
 
 user_yey = input("Do you know how many jumps?(Y/N)\n")
 if user_yey == "y" or user_yey == "Y":
     user_jump = input("How many jumps?(Write a number)\n")
-    print(user_jump)
-    print("end")
+   
+    for x, i in enumerate(encrypted_message):
+        for z, e in enumerate(alfabet):
+           if i == alfabet[z]:
+             decrypted_message[x] = alfabet[e+user_jump] 
+
     
 
 print(encrypted_message)
+print(decrypted_message)
+
+
+#gjør om alle bokstaver til tall, legg på antall hopp til tallet, converter tilbake til bokstav.
 
 #try 
 #if user_yey == Y: 
