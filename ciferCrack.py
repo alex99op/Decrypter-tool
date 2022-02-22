6659588bacfaa0941bed74827d05f554f86f0f02
alex99op <alexander.ronasen@gmail.com>
2022-02-22 10:22:03 +0100

    V0.01
    Not working, but uploaded.

---
+++ ciferCrack.py
@@ -0,0 +1,19 @@
+
+alfabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+user_password = input("Skriv in passord\n")
+user_password = list(user_password) #Her blir stringen gjort om til en array.
+user_yey = input("\nVet du hvor mange hopp?(Y/N)\n")
+user_hopp = input("Hvor mange hopp?\n")
+new_password = []
+
+
+#try 
+#if user_yey == Y: 
+    
+#else user_yey == N:
+#    bruteforce
+
+#for x, i in enumerate (user_password):
+ #   if i == user_password[x]:
+  #      print("Hello")
