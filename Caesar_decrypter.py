a932af08dd05892fe54ead60b96c3a01a950500a
alex99op <alexander.ronasen@gmail.com>
2022-02-23 08:29:35 +0100

    Caesar decrypter v.0.1.2
    Made a fix for space bug.

--- Caesar_decrypter.py
+++ Caesar_decrypter.py
@@ -4,6 +4,7 @@ decrypted_message = []
 
 def bytterf(user_jump):
 	for i in encrypted_message:
+<<<<<<< Updated upstream
 		buff.append(ord(i) -(96-user_jump))
 	byttert()
 
@@ -12,6 +13,21 @@ def byttert():
 	for x, i in enumerate(buff):
 		decrypted_message.append(chr(i + 96))
 
+=======
+		buff.append(ord(i) - (96 - user_jump))
+	convert_to_char(user_jump)
+
+
+def convert_to_char(user_jump):
+	"""Converts ASCII value to letters"""
+	for i in buff:
+		print(i)
+		if i == (-64+user_jump): #Hvis jeg plusser inn hoppet her så vil den muligens bli modulær for å få space uansett antall hopp?
+			decrypted_message.append(chr(32))
+		else:
+			decrypted_message.append(chr(i + 96))
+   
+>>>>>>> Stashed changes
 
 encrypted_message = input("Write inn the encrypted message\n")
 user_yey = input("Do you know how many jumps?(Y/N)\n")
@@ -21,11 +37,17 @@ if user_yey == "y" or user_yey == "Y":
 	user_jump = int(input("How many jumps?(Write a number)\n"))
 	bytterf(user_jump)
 
-
+"""
 for x, i in enumerate(decrypted_message):
+<<<<<<< Updated upstream
 	if i == '!':
 		decrypted_message[x] = chr(32)
 
+=======
+		if i == '!':
+			decrypted_message[x] = chr(32)
+"""
+>>>>>>> Stashed changes
         
 print(''.join(decrypted_message))
 
