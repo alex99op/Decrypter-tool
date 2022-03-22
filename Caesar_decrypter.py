e539c9b87dca30820cbc413616d6beae0ecbff44
alex99op <alexander.ronasen@gmail.com>
2022-03-22 08:21:18 +0100

    Visuals v0.1
    Some visuals to be more user-friendly.

--- Caesar_decrypter.py
+++ Caesar_decrypter.py
@@ -26,18 +26,32 @@ def convert_to_char(user_jump):
 		else:
 			decrypted_message.append(chr(i + 96))
 
+
 encrypted_message = input("Write inn the encrypted message\n")
 user_yey = input("Do you know how many jumps?(Y/N)\n")
-
 
 if user_yey == "y" or user_yey == "Y":
 	user_jump = int(input("How many jumps?(Write a number)\n"))
 	convert_from_char(user_jump)
-        
-print(''.join(decrypted_message))
+	print(''.join(decrypted_message))      
 
-#gjør om alle bokstaver til tall, legg på antall hopp til tallet, converter tilbake til bokstav.
-#Sette opp som funksjoner for å gjøre ting mer modulært.
+elif user_yey == "n" or user_yey == "N":
+	user_jump = 1
+	print("We will try up to 10 jumps:")
+ 
+	while user_jump < 11:
+		convert_from_char(user_jump)
+		print(f"{user_jump}." + ''.join(decrypted_message))
+		user_jump += 1
+		decrypted_message = []
+		buff = []
+else:
+	user_yey = input("I did not understand that, Do you know how many jumps?(Y/N)\n")
+
+
+
+#gjør om alle bokstaver til tall, legg på antall hopp til tallet, converter tilbake til bokstav. DONE!
+#Sette opp som funksjoner for å gjøre ting mer modulært. Sette opp Velkomstmeny? og som en funksjon så man kan komme tilbake til den.
 
 #try 
 #if user_yey == Y: 
---
+++ lekeplass.py
@@ -0,0 +1,42 @@
+def decrypt_menu():
+	print("Simple decrypt will just decrypt with letters, advanced will decypt with letters but also with the use of spesial charackters\n1.Simple\n2.Advanced\n'q' to quit\n")
+	#Legg inn try else for en fin loop.	
+	user_input = input()
+	
+ 
+def main_menu():
+	""" This is the Main manu where it all starts """
+	print("#######################")
+	print("# Welcome to OFFCRYPT #")
+	print("#######################")
+	print('Press "q" to quit\n')
+	print("1. Decrypt")
+	print("2. Encrypt")
+	print("3. Help")
+ 
+	user_input = input()
+
+	if user_input == "1":
+		#Go to Decrypt file/funksjonen
+		print("")
+  
+	elif user_input == "2":
+		#Go to Encrypt file/funksjon
+		print("")
+  
+	elif user_input == "q" or user_input == "Q":
+		quit()
+  
+	elif user_input == "3":
+		print("Wiki\n This software is just for learning and nothing else. Written by AWR, free for all open source software.")
+  
+	else:
+		print("Did not understand that, going back to main menu")
+		#sleep 1 sekund
+		#clear screen
+		main_menu()
+  
+  
+
+ 
+main_menu()
